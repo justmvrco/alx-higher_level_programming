d153416f22cc9a6f6abd90e87ee28d900c0aeec2
justmvrco <moreirapiliki@gmail.com>
2021-09-07 20:26:55 +0000

    marco

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
-
+for c in range(97, 123):
+    print("{:c}".format(c), end=""
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i == 113 or i == 101:
-        continue
-    print("{:c}".format(i), end="")
-
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
