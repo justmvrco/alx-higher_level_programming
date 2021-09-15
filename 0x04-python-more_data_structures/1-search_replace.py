59012e354a8d98044358bdb095540779155a9dd4
justmvrco <moreirapiliki@gmail.com>
2021-09-15 09:09:39 -0700

    Squared simple

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+# replaces all occurences of an element by another.
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
