d0f0e84dc2d8372b672ddf4f683ef5a329168615
justmvrco <moreirapiliki@gmail.com>
2021-11-16 12:49:07 -0800

    add 09

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
+
