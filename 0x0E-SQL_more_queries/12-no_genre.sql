b9ab1a3e2a02de914ad74af2e7b7ab5ba6893954
justmvrco <moreirapiliki@gmail.com>
2021-11-17 12:36:29 -0800

    add task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+       FROM tv_shows
+       LEFT OUTER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       WHERE tv_show_genres.show_id IS NULL
+       ORDER BY tv_shows.title, tv_show_genres.genre_id;
