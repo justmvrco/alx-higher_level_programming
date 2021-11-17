313040bb980279f435794cced65bef56c4abd7c2
justmvrco <moreirapiliki@gmail.com>
2021-11-17 12:41:49 -0800

    add task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all genres the show dexter is listed under
+SELECT tv_shows.title, tv_genres.name 
+       FROM tv_shows
+       LEFT OUTER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       	  LEFT OUTER JOIN tv_genres
+	  ON tv_show_genres.genre_id = tv_genres.id
+       ORDER BY tv_shows.title, tv_genres.name;
+
