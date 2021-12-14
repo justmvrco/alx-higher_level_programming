6ff2680193d27572e9c433ce2accd6f164f27390
justmvrco <moreirapiliki@gmail.com>
2021-12-14 12:21:49 +0000

    add task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// script that creates a class.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
